d816d09bc55496a57de2d3bf069581ba37d10174
rfaeel <rafolomaster@gmail.com>
2025-03-13 20:32:05 -0300

    Resolved merge conflicts

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
 import { useSession } from "next-auth/react"
+
+import { SettingsSchema } from "@/schemas";
 
 import {
     Card,
@@ -11,10 +16,28 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { settings } from "@/actions/settings";
+import {
+    Form,
+    FormField,
+    FormControl,
+    FormItem,
+    FormLabel,
+    FormDescription,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const SettingsPage = () => {
     const { update } = useSession();
     const [isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof SettingsSchema>>({
+        resolver: zodResolver(SettingsSchema),
+        defaultValues:{
+            name: ""
+        }
+    });
+
     const onClick = () => {
         startTransition(() => {
         settings({
